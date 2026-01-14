2a1c4b485cdc8e5e842348a8b1c1ea82980e64cb
victor.alban <victor.alban@izertis.com>
2026-01-14 15:31:38 +0100

    feat(shell): Revamp root page layout and styles with new design tokens
    - Replaced <app-nx-welcome /> component with a custom welcome section in root.page.html.
    - Enhanced the overall layout with a structured container and added hero, middle content, and commands sections.
    - Updated root.page.scss to include comprehensive styles for the new layout, utilizing design tokens for consistency.
    - Removed NxWelcomeComponent from root.page.ts as it is no longer used.
    - Refined global styles in styles.scss to improve typography, reset styles, and introduce utility classes for shadows and rounded corners.

--- apps/shell/src/app/core/ui/pages/root-page/root.page.ts
+++ apps/shell/src/app/core/ui/pages/root-page/root.page.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NxWelcomeComponent } from '../../components/nx-welcome/nx-welcome.component';
 
 @Component({
   selector: 'app-root.page',
   standalone: true,
-  imports: [CommonModule, NxWelcomeComponent],
+  imports: [CommonModule],
   templateUrl: './root.page.html',
   styleUrl: './root.page.scss',
 })
